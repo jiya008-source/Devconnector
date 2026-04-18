67e98f844799e25710d4ffdfa5e8f47a2ea0f0a7
Jiya Arora <jiyaarora1250@gmail.com>
2026-04-18 17:21:39 +0530

    fixed

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -9,47 +9,38 @@ import DashboardActions from './DashboardActions';
 import Experience from './Experience';
 import Education from './Education';
 
-const Dashboard = ({getCurrentProfile,deleteAccount,auth:{user},profile:{profile,loading}}) => {
-    useEffect(()=>{
+const Dashboard = ({getCurrentProfile, deleteAccount, auth:{user}, profile:{profile, loading}}) => {
+    useEffect(() => {
         getCurrentProfile();
-    },[getCurrentProfile]);
-  return loading && profile===null?<Spinner />:<Fragment>
-    <h1 className="large text-primary">Dashboard</h1>
-    <p className="lead">
-    <i className="fas fa-user"></i>Welcome {user&&user.name}</p>
-    
-   {
-  profile && Object.keys(profile).length > 0 ? (
-    <Fragment>
-      <DashboardActions/>
-      <Experience experience={profile.experience}/>
-      <Education education={profile.education}/>
-      <div className='my-2'>
-        <button className='btn btn-danger' onClick={()=>deleteAccount()}>
-        <i className='fas fa-user-minus'></i> Delete my account </button> 
-      </div>
-    </Fragment>
-  ) : (
-    <Fragment>
-      <p>You have not yet set up a profile, please set some info</p>
-      <Link to="/create-profile" className="btn btn-primary my-1">
-        Create Profile
-      </Link>
-    </Fragment>
-  )
-}
-  </Fragment>;
-}
+    }, [getCurrentProfile]);
 
-Dashboard.propTypes = {
-  getCurrentProfile:PropTypes.func.isRequired,
-  auth:PropTypes.object.isRequired,
-  profile:PropTypes.object.isRequired,
-  deleteAccount:PropTypes.func.isRequired
-}
-const mapStateToProps=state=>({
-    auth:state.auth,
-    profile:state.profile
-})
+    if (loading || !user) return <Spinner />;
 
-export default connect(mapStateToProps,{getCurrentProfile,deleteAccount})(Dashboard);
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Dashboard</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i> Welcome {user.name}
+            </p>
+            {profile && Object.keys(profile).length > 0 ? (
+                <Fragment>
+                    <DashboardActions />
+                    <Experience experience={profile.experience} />
+                    <Education education={profile.education} />
+                    <div className='my-2'>
+                        <button className='btn btn-danger' onClick={() => deleteAccount()}>
+                            <i className='fas fa-user-minus'></i> Delete my account
+                        </button>
+                    </div>
+                </Fragment>
+            ) : (
+                <Fragment>
+                    <p>You have not yet set up a profile, please set some info</p>
+                    <Link to="/create-profile" className="btn btn-primary my-1">
+                        Create Profile
+                    </Link>
+                </Fragment>
+            )}
+        </Fragment>
+    );
+};
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -7,47 +7,48 @@ import {
     LOGIN_FAIL,
     LOGOUT,
     ACCOUNT_DELETED
-}from '../actions/types';
+} from '../actions/types';
 
-const initialState={
-    token:localStorage.getItem('token'),
-    isAuthenticated:null,
-    loading:true,
-    user:null
+const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    user: null
+};
+
+export default function(state = initialState, action) {
+    const { type, payload } = action;
+    switch(type) {
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            };
+        case REGISTER_SUCCESS:
+        case LOGIN_SUCCESS:
+            localStorage.setItem('token', payload.token);
+            return {
+                ...state,
+                token: payload.token,
+                isAuthenticated: true,
+                loading: true  // stays true until USER_LOADED
+            };
+        case REGISTER_FAIL:
+        case LOGIN_FAIL:
+        case AUTH_ERROR:
+        case LOGOUT:
+        case ACCOUNT_DELETED:
+            localStorage.removeItem('token');
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false,
+                user: null
+            };
+        default:
+            return state;
+    }
 }
-export default function(state=initialState,action){
-    const {type,payload}=action;
-    switch(type){
-    case USER_LOADED:
-        return {
-            ...state,
-            isAuthenticated:true,
-            loading:false,
-            user:payload
-        };
-    case REGISTER_SUCCESS:
-    case LOGIN_SUCCESS:
-        localStorage.setItem('token',payload.token);
-        return{
-            ...state,
-            ...payload,
-            isAuthenticated:true,
-            loading:false
-        };
-    case REGISTER_FAIL:
-    case LOGIN_FAIL:
-    case AUTH_ERROR:
-    case LOGOUT:
-    case ACCOUNT_DELETED:
-        localStorage.removeItem('token');
-        return{
-            ...state,
-            token: null,
-            isAuthenticated:false,
-            loading:false,
-            user: null
-        };
-    default:
-        return state;
-
-}}
